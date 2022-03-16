22c3be5a498403f98486c0637bccfc517264a5fd
Hernan Alvarez Caballero <hernandavidc@hotmail.com>
2022-03-16 16:34:00 -0500

    rr

--- app/assets/SubMenuHeaderDashEmpty.0fcecf47.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./index.502c7d60.js";import{d as o,o as r,j as s}from"./vendor.c408dfb7.js";const p=o({setup(c){return(n,t)=>(r(),s(e))}});export{p as default};
---
+++ app/assets/SubMenuHeaderDashEmpty.ae60c86a.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index.be7187f0.js";import{d as o,o as r,j as s}from"./vendor.c408dfb7.js";const f=o({setup(n){return(t,a)=>(r(),s(e))}});export{f as default};
